2f54463a6568579cf0867f6006acb5485081e642
Francesco <francesco.falchi@gmail.com>
2017-12-13 21:12:02 +0100

    Generate providers for person and cooper, populate them

---
+++ src/providers/person/person.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+import { CooperProvider } from '../cooper/cooper';
+
+@Injectable()
+export class PersonProvider {
+  public gender: string;
+  public age: number;
+  public assessmentMessage: string;
+
+  constructor(private cooper: CooperProvider) {}
+
+  doAssessment(distance: number): void {
+    this.assessmentMessage = this.cooper.assess(this, distance);
+  }
+
+}
