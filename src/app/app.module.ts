f507c046500bc0b9d020d75b7325e1e22fcaf023
Francesco <francesco.falchi@gmail.com>
2017-12-14 21:45:08 +0100

    Generate page results, show as modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ResultsPage } from '../pages/results/results';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,6 +19,7 @@ import { PerformanceDataProvider } from '../providers/performance-data/performan
   declarations: [
     MyApp,
     HomePage,
+    ResultsPage
   ],
   imports: [
     BrowserModule,
@@ -29,6 +31,7 @@ import { PerformanceDataProvider } from '../providers/performance-data/performan
   entryComponents: [
     MyApp,
     HomePage,
+    ResultsPage
   ],
   providers: [
     StatusBar,
--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
 
 @Component({
   selector: 'page-results',
   templateUrl: 'results.html',
 })
 export class ResultsPage {
+  results = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private perfomanceData: PerformanceDataProvider,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ResultsPage');
+    this.perfomanceData
+      .getResults()
+      .subscribe(data => (this.results = data.entries));
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
